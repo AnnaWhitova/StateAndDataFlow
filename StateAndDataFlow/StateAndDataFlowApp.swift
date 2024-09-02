be20a240c0326a602f841c3510f86676743c05a0
AnnaWhitova <159627315+AnnaWhitova@users.noreply.github.com>
2024-09-02 18:09:49 +0300

    Initial Commit

---
+++ StateAndDataFlow/StateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowApp.swift
+//  StateAndDataFlow
+//
+//  Created by Анна Белова on 02.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
