4739c9e2f3f309c07d28efb4188fe059335f133e
AnnaWhitova <159627315+AnnaWhitova@users.noreply.github.com>
2024-09-08 21:02:25 +0300

    create storage

--- StateAndDataFlow/Services/StorageManager.swift
+++ StateAndDataFlow/Services/StorageManager.swift
@@ -7,21 +7,28 @@
 
 import SwiftUI
 
-final class StorageManager: ObservableObject {
+final class StorageManager {
     static let shared = StorageManager()
 
-    @AppStorage("userName") var userName: String = ""
-    @AppStorage("isLoggedIn") var isLoggedIn: Bool = false
+    @AppStorage("userData") var userData: Data?
+    
+    private var decoder = JSONDecoder()
+    private var encoder = JSONEncoder()
 
-    private init() { }
+    private init() {}
 
-    func saveUser(name: String) {
-        userName = name
-        isLoggedIn = true
+    func saveUser(_ user: User) {
+        if let encoded = try? encoder.encode(user) {
+            userData = encoded
+        }
     }
 
-    func logout() {
-        userName = ""
-        isLoggedIn = false
+    func FetchUser() -> User? {
+        guard let userData = userData else {return nil}
+        return try? decoder.decode(User.self, from: userData)
+    }
+    
+    func logOut(_ userManager: UserManager) {
+        userManager.user.isLoggedIn = false
     }
 }
